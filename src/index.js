097a4b99c295386d96168535e4ae453bbcce776f
mawa <mawahama05@gmail.com>
2022-11-17 16:25:33 -0800

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.getElementsByTagName('form').addEventListener('submit',(e)=>{
+     e.preventDefault()
+     const placeholder = document.querySelector('#new-task-description')
+
+     addToDo(placeholder.value)
+  })
+      const addToDo = (item)=>{
+      const item = document.createElement('p')
+          document.querySelector('#tasks').appendChild(p)
+      }
+}
+)
